f66ac85111c8f8fc60eb884c9e8e3180bab8432e
Grant Levene <glevene@arbor.net>
2018-12-02 12:24:14 -0500

    Day 2

---
+++ 02/main.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+
+from functools import reduce
+from itertools import combinations
+
+def getLines():
+    with open(os.path.join(sys.path[0], 'input.txt')) as f:
+        return [l.strip() for l in f.readlines()]
+
+def p1(ids):
+    return reduce(
+        lambda x, y: x * y,
+        [sum(1 for l in ids if any(l.count(c) == x for c in l)) for x in (2,3)]
+    )
+
+def p2(ids):
+    for first, second in combinations(ids, 2):
+        z = list(zip(first, second))
+        nDiffs = len([1 for f, s in z if f != s])
+
+        if nDiffs == 1:
+            return ''.join([f for f, s in z if f == s])
+
+def main():
+    ids = getLines()
+
+    print('P1: {}'.format(p1(ids)))
+    print('P2: {}'.format(p2(ids)))
+
+if __name__ == "__main__":
+    main()
